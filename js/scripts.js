5aca073d5555f49f2dbf4f292277ac81ea5165cf
Debbie Pacheco <pachecodeb@yahoo.com>
2019-09-17 18:41:08 -0700

    create list function

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+//get the element container, create data attribute
+const listsContainer = document.querySelector('[data-lists]');
+//select list items select elements
+const newListForm = document.querySelector('[data-new-list-form]');
+const newListInput = document.querySelector('[data-new-list-input]');
+
+
+//create variable to hold list
+let lists = [{
+    id: 1,
+    name: 'name'
+}, {
+    id: 2,
+    name: 'todo'
+}]
+
+//setup eventlistener for new list form
+newListForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const listName = newListInput.value 
+    if (listName == null || listName ==='') return
+    const list = createList(listName)
+})
+
+
+//create render function and clear element function toclear list
+function render() {
+    clearElement(listsContainer)
+    lists.forEach(list => {
+        const listElement = document.createElement('li')
+        listElement.dataset.listId = list.id
+        listElement.classList.add("list-name")
+        listElement.innerText = list.name
+        listsContainer.appendChild(listElement)
+    })
+}
+
+//setup while loop
+function clearElement(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild)
+    }
+}
+
+render()
