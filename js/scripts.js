f709f4144432e7485d8e9587565b97ff1163ec6c
Debbie Pacheco <pachecodeb@yahoo.com>
2019-09-18 00:15:05 -0700

    to do list is wonky will try to fix tomorrow

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,32 @@ const listsContainer = document.querySelector('[data-lists]');
 //select list items select elements
 const newListForm = document.querySelector('[data-new-list-form]');
 const newListInput = document.querySelector('[data-new-list-input]');
+const deleteListButton = document.querySelector('[data-delete-list-button]')
+const listDisplayContainer = document.querySelector('[data-lists-display-container]')
+const listTitleElement = document.querySelector('[data-list-title]')
+const listCountElement = document.querySelector('[data-lists-count]')
+const tasksContainer = document.querySelector('[data-tasks]')
+const taskTemplate = document.getElementById(['task-template'])
 
+//create variable to hold list using localstorage
+const LOCAL_STORAGE_LIST_KEY = 'task.lists'
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId'
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
 
-//create variable to hold list
-let lists = [{
-    id: 1,
-    name: 'name'
-}, {
-    id: 2,
-    name: 'todo'
-}]
+//set up event listener to container 
+listsContainer.addEventListener('click', e => {
+    if (e.target.tagName.toLowerCase() === 'li'){
+        selectedListId = e.target.dataset.listId
+        saveAndRender()
+    }
+} )
 
+deleteListButton.addEventListener('click', e => {
+    lists = lists.filter(list => list.id !== selectedListId)
+    selectedListId = null
+    saveAndRender()
+})
 //setup eventlistener for new list form
 newListForm.addEventListener('submit', e => {
     e.preventDefault()
@@ -22,21 +37,70 @@ newListForm.addEventListener('submit', e => {
     const list = createList(listName)
     newListInput.value = null
     lists.push(list)
-    render()
+    saveAndRender()
 })
 
 function createList(name) {
-    return { id: Date.now().toString(), name: name, tasks:[]}
+    return { id: Date.now().toString(), name: name, tasks: [] }
+}
+
+function saveAndRender(){
+    save()
+    render()
+}
+
+function save() {
+    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
+    localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId)
 }
 
 //create render function and clear element function toclear list
 function render() {
     clearElement(listsContainer)
+    renderLists()
+    const selectedList = lists.find(list => list.id === selectedListId)
+    if (selectedListId == null) {
+        listDisplayContainer.style.display = 'none'
+
+    } else {
+        listDisplayContainer.style.display = ''
+        listTitleElement.innerText = selectedListId.name
+        renderTaskCount(selectedList)
+        clearElement(tasksContainer)
+        renderTasks(selectedList)
+
+    }
+}
+
+function renderTasks(selectedListId) {
+    selectedList.tasks.forEach(task => {
+        const taskElement = document.importNode(taskTemplate.contentEditable, true)
+        const checkbox = taskElement.querySelector('input')
+        checkbox.id = task.id
+        checkbox.checked = task.complete
+        const label = taskElement.querySelector('label')
+        label.htmlFor = task.id
+        label.append(task.name)
+        tasksContainer.appendChild(taskElement)
+
+
+    })
+}
+function renderTaskCount(selectedList) {
+const incompleteTaskCount = selectedListId.tasks.filter(task => !task.complete).length
+const taskString = incompleteTaskCount === 1 ? "task" : "tasks"
+listCountElement.innerText = `${incompleteTaskCount} ${taskString} remaining`
+}
+
+function renderLists () {
     lists.forEach(list => {
         const listElement = document.createElement('li')
         listElement.dataset.listId = list.id
         listElement.classList.add("list-name")
         listElement.innerText = list.name
+        if (list.id === selectedListId) {
+            listElement.classList.add('active-list')
+        }
         listsContainer.appendChild(listElement)
     })
 }
